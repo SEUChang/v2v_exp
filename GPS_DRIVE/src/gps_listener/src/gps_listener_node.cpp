ca019a79c19ec682cfec940b039040bcfe44882e
CastielLiu <castiel_liu@outlook.com>
2020-07-19 17:53:12 +0800

    gps test successful

--- GPS_DRIVE/src/gps_listener/src/gps_listener_node.cpp
+++ GPS_DRIVE/src/gps_listener/src/gps_listener_node.cpp
@@ -7,14 +7,15 @@ void gpsCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {  
     nav_msgs::Odometry ll2utm_msg;
     ll2utm_msg = *msg;
-    ROS_INFO("UTC: .2f%",ll2utm_msg.twist.twist.linear.y);
-    ROS_INFO("utm x : .2f%",ll2utm_msg.pose.pose.position.x);
-    ROS_INFO("utm y : .2f%",ll2utm_msg.pose.pose.position.y);
-    ROS_INFO("longitude : .2f%",ll2utm_msg.pose.covariance[0]);
-    ROS_INFO("latitude : .2f%",ll2utm_msg.pose.covariance[1]);
-    ROS_INFO("yaw (rad) : .2f",ll2utm_msg.pose.covariance[2]);
-    ROS_INFO("speed vx (m/s): .2f%",ll2utm_msg.twist.twist.linear.x); 
-    ROS_INFO("==========end==========");
+    ROS_INFO("UTC: %.2f",ll2utm_msg.twist.twist.linear.y);
+    ROS_INFO("utm x : %.2f",ll2utm_msg.pose.pose.position.x);
+    ROS_INFO("utm y : %.2f",ll2utm_msg.pose.pose.position.y);
+    ROS_INFO("longitude : %f",ll2utm_msg.pose.covariance[0]);
+    ROS_INFO("latitude : %f",ll2utm_msg.pose.covariance[1]);
+    ROS_INFO("yaw (rad) : %f",ll2utm_msg.pose.covariance[2]);
+    ROS_INFO("yaw (deg) : %f",ll2utm_msg.pose.covariance[2] / 3.1415926 * 180);
+    ROS_INFO("speed vx (m/s): %.2f",ll2utm_msg.twist.twist.linear.x);
+    ROS_INFO("========================="); 
 }
 
 int main(int argc, char **argv)
